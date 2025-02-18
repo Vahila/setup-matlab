89a83f1459ae37b54bb0374d0c27881c4eaea566
Vahila <70003902+Vahila@users.noreply.github.com>
2025-02-18 14:18:25 +0530

    Update mpm.ts

--- src/mpm.ts
+++ src/mpm.ts
@@ -4,6 +4,7 @@ import * as exec from "@actions/exec";
 import * as tc from "@actions/tool-cache";
 import {rmRF} from "@actions/io";
 import * as path from "path";
+import * as fs from 'fs';
 import * as matlab from "./matlab";
 import properties from "./properties.json";
 
